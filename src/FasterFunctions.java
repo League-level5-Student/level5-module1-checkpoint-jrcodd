817f9d22f21e70a4106a2fa846a681128f90a0e8
league <league@imac-27.attlocal.net>
2020-03-10 16:21:26 -0700

    Checkpoint lvl 5 mod 1

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -1,14 +1,27 @@
+import java.util.ArrayList;
 
 public class FasterFunctions {
 	public static void main(String[] args) {
 		float totalTime = 0;
 		long startTime = System.currentTimeMillis();
-		//Your Code Here
-		
-		
-		
-		
-		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+		// Your Code Here
+		ArrayList<Thread> threads = new ArrayList<Thread>();
+		Thread t1 = new Thread(() -> {
+			SlowFunctions.slowSortLargeArray();
+		});
+		Thread t2 = new Thread(() ->{
+			SlowFunctions.ackermann(3, 14);
+		});
+		Thread t3 = new Thread(()-> {
+			SlowFunctions.millionsOfSqrts();
+		});
+		threads.add(t1);
+		threads.add(t2);
+		threads.add(t3);
+		for (Thread thread : threads) {
+			thread.start();
+		}
+		float endTime = (float) ((double) System.currentTimeMillis() - (double) startTime) / 1000.0f;
 		totalTime += endTime;
 		System.out.println("Total Time: " + totalTime);
 	}
